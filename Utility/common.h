76d9fdf535013ac167de8a86ec7c4cd2fa5141f6
dorayo <yomingyo@gmail.com>
2015-06-25 21:00:57 +0800

    增加从main bundle中获取资源文件路径的宏和根据main bundle中资源文件路径来生成不缓存UIImage对象的宏

--- Utility/common.h
+++ Utility/common.h
@@ -1,11 +1,21 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-// 封装NSLog
+// 00.封装NSLog
 #ifdef DEBUG // 调试版本
 #define YMLog(...) NSLog(__VA_ARGS__)
 #else // 发布版本
 #define YMLog(...) 
 #endif
+
+// 01.取出main bundle中的资源的路径
+#define ResourcePath(path)  [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:path]
+
+// 02.根据指定路径从main bundle中加载生成无缓存的图片(UIImage)对象
+#define ImageWithPath(path) [[UIImage alloc] initWithContentsOfFile:path]
+/* 01和02可以组合使用
+ * eg: UIImage *image = ImageWithPath(ResourcePath(@"abc/123.jpg"));
+ * 以上代码，将main bundle的abc目录下的123.jpg生成了不缓存的UIImage对象
+ */
 
 #endif
