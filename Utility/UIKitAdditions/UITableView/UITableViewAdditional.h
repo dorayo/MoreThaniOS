4dda9d71925b3c380baf3264efc718f8f60f2a0e
jiwei <weiji@weji.info>
2015-06-26 22:43:30 +0800

    uitableView indexPath 转为index

---
+++ Utility/UIKitAdditions/UITableView/UITableViewAdditional.h
@@ -0,0 +1,19 @@
+//
+//  UITableViewAdditional.h
+//  iWeiBo
+//
+//  Created by jiwei on 14/12/4.
+//  Copyright (c) 2014年 weiji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  UITableView的一个将IndexPath和Index相互转化的扩展
+ */
+
+@interface UITableView (Addtional)
+
+-(NSInteger)index4IndexPath:(NSIndexPath *)indexPath;
+
+@end
